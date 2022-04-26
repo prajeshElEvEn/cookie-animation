457794910fa9ec73b0c628a4ef7e59d514da78aa
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-04-26 23:04:59 +0530

    - added README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Cookie Popup Animation
+
+Just a cookie popup animation using [GSAP](https://greensock.com/gsap/). GSAP CDN can be found [here](https://cdnjs.com/libraries/gsap).
+
+- GSAP CDN
+
+```bash
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js" integrity="sha512-VEBjfxWUOyzl0bAwh4gdLEaQyDYPvLrZql3pw1ifgb6fhEvZl9iDDehwHZ+dsMzA0Jfww8Xt7COSZuJ/slxc4Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+```
+
+## Reference
+
+[Dev Ed](https://www.youtube.com/watch?v=r1iul4uRFuE)
+
+## Author
+
+[@prajeshElEvEn](https://github.com/prajeshElEvEn)
--- src/app.js
+++ src/app.js
@@ -47,15 +47,73 @@ tl.fromTo(
     '<'
 )
 
+tl.fromTo(
+    '.cookie',
+    {
+        y: 0,
+        rotation: '0deg'
+    },
+    {
+        y: -10,
+        rotation: '-10deg',
+        yoyo: true,
+        repeat: -1,
+        duration: 0.5
+    }
+)
+tl.fromTo(
+    '#crumbs',
+    {
+        y: 0,
+        // rotation: '0deg'
+    },
+    {
+        y: -10,
+        // rotation: '-10deg',
+        yoyo: true,
+        repeat: -1,
+        duration: 0.5
+    },
+    '<'
+)
+
 const zip = () => {
+    // tl.fromTo(
+    //     '.cookie',
+    //     {
+    //         opacity: 1,
+    //         x: 0,
+    //         rotation: '0deg'
+    //     },
+    //     {
+    //         opacity: 0,
+    //         x: -100,
+    //         rotation: '-45deg'
+    //     }
+    // )
+    // tl.fromTo(
+    //     '.text',
+    //     {
+    //         opacity: 1,
+    //         x: 0
+    //     },
+    //     {
+    //         opacity: 0,
+    //         x: 100
+    //     },
+    //     '<'
+    // )
     tl.fromTo(
         '.popup',
         {
+            opacity: 1,
             scale: 1,
         },
         {
+            opacity: 0,
             scale: 0,
             duration: 0.3,
-        }
+        },
+        // '<50%'
     )
 }
